b12a30fc78c0eb5bdc6e5c6daaa170ef3b6c341c
杨明照 <yangmingzhao@interlib.top>
2020-09-18 17:08:02 +0800

    feat: 配置基础完成

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
   configureWebpack:{
     resolve:{
@@ -7,6 +9,7 @@ module.exports = {
           'common':'@/common',
           'components':'@/components',
           'network':'@/network',
+          'vue$': 'vue/dist/vue.esm.js',
           // 'rounter':'@/rounter'只在main.js引用，直接用，this.$rounter
           'views':'@/views',
           // 'store':'@/store',和rounter一样
@@ -18,17 +21,17 @@ module.exports = {
       sass: {
         prependData: '@import "~@/assets/common.scss";'
       },
-      postcss: {
-        plugins: [
-          autoprefixer(),
-          pxtorem({
-            rootValue: 75, // 换算的基数(设计图750的根字体为32)
-            selectorBlackList: [".van", ".not-rem"], // 要忽略的选择器并保留为px。
-            propList: ["*"],
-            minPixelValue: 2 // 设置要替换的最小像素值。
-          })
-        ]
-      }
+      // postcss: {
+      //   plugins: [
+      //     autoprefixer(),
+      //     pxtorem({
+      //       rootValue: 75, // 换算的基数(设计图750的根字体为32)
+      //       selectorBlackList: [".van", ".not-rem"], // 要忽略的选择器并保留为px。
+      //       propList: ["*"],
+      //       minPixelValue: 2 // 设置要替换的最小像素值。
+      //     })
+      //   ]
+      // }
     }
   },
   devServer: {
