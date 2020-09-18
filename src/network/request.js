74caa7217b50f8f11a736c611aa077cbdbb0f1b0
杨明照 <yangmingzhao@interlib.top>
2020-09-18 17:27:57 +0800

    feat: 配置路由

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 1.配置这个模块特有的基础配置
 const instant = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
   },
